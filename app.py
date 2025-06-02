fc419eb5e06f580ac8200bbf1ee38ba8c058a50d
strimapp <kontaksaham101@gmail.com>
2025-06-02 11:24:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import os
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_PORT = os.getenv("DB_PORT", 5432)
-
+DB_HOST = os.getenv("PGHOST")
+DB_NAME = os.getenv("PGDATABASE")
+DB_USER = os.getenv("PGUSER")
+DB_PASS = os.getenv("PGPASSWORD")
+DB_PORT = os.getenv("PGPORT", 5432)
 
 def get_conn():
     return psycopg2.connect(
